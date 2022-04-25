ad3d1c8cd3b3896fe32200f24a34d31b5d314b2d
Sanjay Nair <sanjayhnair@gmail.com>
2022-04-24 21:25:49 -0400

    Updating brew deps. Splitting linux install

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,8 +1,13 @@
 
 # Install Homebrew
+echo "Installing Homebrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew bundle
 
 echo "Setup:: global git config"
 cp .gitconfig $HOME
+
+# Install nvm
+echo "Installing NVM"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
